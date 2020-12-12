9c5bc633e369c27267ed6b307bca12add2feffc3
Kiprin Konstantin <kiprin.konstantin@yandex.ru>
2020-12-13 05:43:47 +0700

    пр12 трай 1.0

---
+++ routes/cards.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const path = require('path');
+const fsPromises = require('fs').promises;
+const pathToFile = path.join(__dirname, '..', 'data', 'cards.json');
+
+const getCards = (req, res) => {
+  return getFiles(pathToFile)
+    .then(data => res.status(200).send(data))
+    .catch(err => res.status(500).send(err));
+};
+
+const getFiles = (pathToFile) => {
+  return fsPromises.readFile(pathToFile, {encoding: 'utf-8'})
+    .then(data => JSON.parse(data))
+    .catch(err => console.log(err));
+}
+
+
+router.get('/cards', getCards);
+
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+const path = require('path');
+const fsPromises = require('fs').promises;
+const pathToFile = path.join(__dirname, '..', 'data', 'users.json');
+
+const getUsers = (req, res) => {
+  return getFiles(pathToFile)
+    .then(data => res.status(200).send(data))
+    .catch(err => res.status(500).send(err));
+};
+
+const getFiles = (pathToFile) => {
+  return fsPromises.readFile(pathToFile, {encoding: 'utf-8'})
+    .then(data => JSON.parse(data))
+    .catch(err => console.log(err));
+}
+
+const getProfile = (req, res) => {
+  return getFiles(pathToFile)
+    .then(users => users.find(users => users._id === req.params.id))
+    .then((user) => {
+      if(!user) {
+        return res.status(404).send('"message": "Нет пользователя с таким id"');
+      }
+      return res.status(200).send(user);
+    })
+    .catch(err => res.status(500).send(err));
+};
+
+router.get('/users', getUsers);
+router.get('/users/:id', getProfile);
+
+module.exports = router;
